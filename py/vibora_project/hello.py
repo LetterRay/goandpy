972717ad7cb017087e3a8953fc533d5c3a3eb100
xiaojie <15189756187@163.com>
2019-03-15 16:34:15 +0800

    fix response type

--- py/vibora_project/hello.py
+++ py/vibora_project/hello.py
@@ -4,7 +4,7 @@ app = Vibora()
 
 @app.route('/')
 async def home():
-    return Response({'hello': 'world'})
+    return Response(f'helloworld'.encode())
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
